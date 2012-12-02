b5854db31a22550c91bd4510596e86afe20ad564
Vasin Dmitriy <vasindima779@gmail.com>
2012-12-02 16:14:24 +0200

    update pg admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121130162440) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "query"
+    t.string   "search_type"
+    t.string   "search_question"
+    t.string   "fined_query",     :default => "f"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+  end
 
 end
